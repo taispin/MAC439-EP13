f867b46d3f1238b9bf25928de3fe37c7df4578cb
Tais Pin <tais141@gmail.com>
2018-06-02 19:35:15 -0300

    inserir professor + aluno

--- src/menu/Interface.java
+++ src/menu/Interface.java
@@ -11,19 +11,17 @@ public class Interface {
 		solicita = new Solicitacao();
 	}
 
-	/************************************************/
-	//Solicitacoes para Professores
-
 	// Adiciona Professor
 	public void adicionaProfessor() {
 		Scanner line = new Scanner(System.in);
 
 		// Recebe os dados do novo professor
-		System.out.println("Informe ops seguintes dados do novo Professor:");
+		System.out.println("Informe os seguintes dados do novo Professor:");
 		System.out.println("ID do professor:");
 		Long id = line.nextLong();
 
 		System.out.println("Nome do Professor:");
+		line.useDelimiter(System.getProperty("line.separator"));
 		String nome = line.next();
 
 		System.out.println("ID do Departamento:");
@@ -32,6 +30,32 @@ public class Interface {
 		System.out.println(solicita.adicionaProfessor(id, nome, depto));
 	}
 
+	// Adiciona Aluno
+	public void adicionaAluno() {
+		Scanner line = new Scanner(System.in);
+
+		// Recebe os dados do novo aluno
+		System.out.println("Informe os seguintes dados do novo Aluno:");
+		System.out.println("Número do Aluno:");
+		Long id = line.nextLong();
+
+		System.out.println("Nome do Aluno:");
+		line.useDelimiter(System.getProperty("line.separator"));
+		String nome = line.next();
+
+		System.out.println("Formacao do Aluno:");
+		line.useDelimiter(System.getProperty("line.separator"));
+		String formacao = line.next();
+
+		System.out.println("Nivel do Aluno:");
+		line.useDelimiter(System.getProperty("line.separator"));
+		String nivel = line.next();
+
+		System.out.println("Idade do Aluno:");
+		int idade = line.nextInt();
+
+		System.out.println(solicita.adicionaAluno(id, nome, formacao, nivel, idade));
+	}
 
 	public static void main(String[] args) {
 
@@ -39,17 +63,22 @@ public class Interface {
 		tela = new Interface();
 		Scanner line = new Scanner(System.in);
 		int escolha;
+		int show = 1;
 
-		while(true) {
+		while(show == 1) {
 			System.out.println("SISTEMA ESCOLA:");
 			System.out.println(" ESCOLHA UMA DAS OPÇÕES:");
 			System.out.println("[1] -> Adicionar um Professor");
-			System.out.println("[2] -> Remover um Professor");
-			System.out.println("[3] -> Alterar um Professor");
+			System.out.println("[2] -> Adicionar um Aluno");
+			System.out.println("[-1] -> SAIR");
 
 			escolha = line.nextInt();
 
 			if(escolha == 1) tela.adicionaProfessor();
+			else if (escolha == 2) tela.adicionaAluno();
+			else if (escolha == -1) show = 0;
+			else System.out.println("Escolha uma opção válida ou -1 para sair!");
+			System.out.println();
 		}
-}
+	}
 }
